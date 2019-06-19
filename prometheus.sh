29bbf5a81f93355b819f0baff72f3e2c6d14c2f5
pamarquez <47809426+pamarquez@users.noreply.github.com>
2019-06-19 21:17:08 +0200

    Update prometheus.sh

--- prometheus.sh
+++ prometheus.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
- useradd -M -s /bin/false prometheus
- useradd --no-create-home -s /bin/false node_exporter
- sudo mkdir /etc/prometheus
- sudo mkdir /var/lib/prometheus
- sudo chown prometheus:prometheus /var/lib/prometheus
- sudo chown prometheus:prometheus /etc/prometheus
- cd /tmp
- wget https://github.com/prometheus/prometheus/releases/download/v2.7.2/prometheus-2.7.2.linux-amd64.tar.gz
- tar xvf prometheus-2.7.2.linux-amd64.tar.gz
- cp prometheus-2.7.2.linux-amd64/prometheus  /usr/local/bin/
- cp prometheus-2.7.2.linux-amd64/promtool /usr/local/bin/
- sudo chown prometheus:prometheus /usr/local/bin/prometheus
- sudo chown prometheus:prometheus /usr/local/bin/promtool
- cp -r prometheus-2.7.2.linux-amd64/consoles /etc/prometheus
- cp -r prometheus-2.7.2.linux-amd64/console_libraries /etc/prometheus
- sudo chown -R prometheus:prometheus /etc/prometheus/
- sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
- cp prometheus-2.7.2.linux-amd64/prometheus.yml /etc/prometheus
- cd prometheus-2.7.2.linux-amd64
- cat prometheus.yml
- cd /etc/systemd/system
- vi prometheus.service
- sudo systemctl daemon-reload
- sudo systemctl start prometheus
- sudo systemctl enable prometheus
- sudo systemctl status prometheus
+ sudo mkdir prometheus
+ cd prometheus
+	wget https://github.com/prometheus/prometheus/releases/download/v2.10.0/prometheus-2.10.0.linux-amd64.tar.gz
+	tar -xzf prometheus-2.10.0.linux-amd64.tar.gz
+	cp /prometheus/prometheus-2.10.0.linux-amd64 /prometheus
+	sudo rm prometheus-2.10.0.linux-amd64/
+ sudo rm prometheus-2.10.0.linux-amd64.tar.gz
+	sudo rm prometheus.yml
+	cp /vagrant-share/prometheus/prometheus.yml prometheus.yml
+	./prometheus &>/dev/null &
+	cd ..
+	
+	sudo mkdir node_exporter
+	cd node_exporter
+	wget https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
+        tar -xzf node_exporter-0.18.1.linux-amd64.tar.gz
+	sudo rm node_exporter-0.18.1.linux-amd64.tar.gz
+	cp /node_exporter/node_exporter-0.18.1.linux-amd64 /node_exporter
+	sudo rm node_exporter-0.18.1.linux-amd64/
+	./node_exporter &>/dev/null &
+	cd ..
+
+	wget https://dl.grafana.com/oss/release/grafana-6.2.2.linux-amd64.tar.gz
+	tar -zxvf grafana-6.2.2.linux-amd64.tar.gz
+	cd grafana-6.2.2/
+	./bin/grafana-server &>/dev/null &
